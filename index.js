f8daf275d21cc7354b73d4e3b072ad68c791334a
JuanRamirezMeLi <juan.ramirezdiaz@mercadolibre.com.co>
2021-12-16 13:06:09 -0500

    ejercicio problema de js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-function remote() {}
+function calculatePrice(item, price) {
+  let total = price;
+  let shipping = 0;
+  if (price > 4000) {
+    shipping = 700;
+  } else if (price > 2000) {
+    shipping = 500;
+  } else {
+    shipping = 300;
+  }
+  total += shipping;
+
+  console.log(
+    `El producto ${item} cuesta ${price}. Su costo de envío es de ${shipping}. 
+    Por lo tanto, el precio final es de ${total}`
+  );
+}
+
+calculatePrice("Macbook", 2500);
+calculatePrice("Celular", 500);
+calculatePrice("Playstation", 4500);
